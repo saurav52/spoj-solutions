e7c0eecf0b74760817944dfa3318910af6369603
saurav52 <shaw1saurav@gmail.com>
2017-06-20 11:32:41 +0530

    Add files via upload

---
+++ FENCE1_spoj.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float l;
+    cin>>l;
+    while(l!=0)
+    {
+        float ans=((l*l)/3.14159)/2;
+         printf("%.2f\n",ans);
+        cin>>l;
+    }
+}
