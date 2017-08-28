75cb01f9c4a3987a861a2f27e5390ba138e73eba
saurav52 <shaw1saurav@gmail.com>
2017-08-28 10:43:39 +0530

    SHAKTI - SHAKTIMAN AND KILWISH

---
+++ SHAKTI_spoj.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,n;
+    cin>>t;
+    for(int z=1;z<=t;z++)
+    {
+        cin>>n;
+        if(n%2==0)
+            cout<<"Thankyou Shaktiman"<<endl;
+        else
+            cout<<"Sorry Shaktiman"<<endl;
+    }
+}
